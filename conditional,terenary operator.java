2e4e1c7cdfe3e95e8a56941ea6eafb55f7d47909
Atif <90785363+atif-tariq@users.noreply.github.com>
2021-12-12 15:38:05 +0530

    Update conditional,terenary operator.java

--- conditional,terenary operator.java
+++ conditional,terenary operator.java
@@ -1,10 +1,15 @@
 /*       CSE-18-50      */
-
+import java.util.*;  
 public class Number {
+    
   public static void main(String args[]) {
-    int i = 69;
-    int j = 50;
-    int result = (i < j) ? i : j;
-    System.out.println(result + " is a minimum value");
+   
+    Scanner sc= new Scanner(System.in); 
+    System.out.print("Enter first number- ");  
+    int a=sc.nextInt();  
+    System.out.print("Enter second number- ");  
+    int b=sc.nextInt();  
+    int result = (a < b) ? a : b;
+    System.out.println(result + " is the minimum value");
   }
 }
