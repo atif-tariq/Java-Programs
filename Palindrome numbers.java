e4fa3790d1c071e4881a4fabe17a46f05f272d54
Atif <90785363+atif-tariq@users.noreply.github.com>
2021-12-12 15:22:59 +0530

    Update Palindrome numbers.java

--- Palindrome numbers.java
+++ Palindrome numbers.java
@@ -1,19 +1,19 @@
 /*       CSE-18-50      */
-
-public class numbers {
-    public static void main(String[] args) {
-        int rem, rev= 0, temp;
-    int n=131; 
-        temp = n;
-        while( n != 0 )
-        {
-            rem= n % 10;
-            rev= rev * 10 + rem;
-            n=n/10;
-        }
-        if (temp == rev)
-            System.out.println(temp + " is a palindrome.");
-        else
-            System.out.println(temp + " is not a palindrome.");
-    }
-}
+import java.util.*;   
+public class Palindrome
+{  
+   public static void main(String args[])  
+   {  
+      String original, reverse = "";  
+      Scanner in = new Scanner(System.in);   
+      System.out.println("Enter a string/number to check if it is a palindrome");  
+      original = in.nextLine();   
+      int length = original.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + original.charAt(i);  
+      if (original.equals(reverse))  
+         System.out.println("Entered string/number is a palindrome.");  
+      else  
+         System.out.println("Entered string/number isn't a palindrome.");   
+   }  
+} 
